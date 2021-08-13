3d7f2bc119f32b60a347487f80fc6f69ed75a9bf
asp-shda <asp-shda@ln.rosenergoatom.ru>
2021-08-13 09:22:38 +0300

    Second commit (day 1)

--- modules/global/src/com/company/pizza/entity/OrderItem.java
+++ modules/global/src/com/company/pizza/entity/OrderItem.java
@@ -2,11 +2,51 @@ package com.company.pizza.entity;
 
 import com.haulmont.cuba.core.entity.StandardEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Table(name = "PIZZA_ORDER_ITEM")
 @Entity(name = "pizza_OrderItem")
 public class OrderItem extends StandardEntity {
     private static final long serialVersionUID = -5129858164112801422L;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "PIZZA_ID")
+    private Pizza pizza;
+
+    @NotNull
+    @Column(name = "QUANTITY", nullable = false)
+    @Min(1)
+    private Integer quantity;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
